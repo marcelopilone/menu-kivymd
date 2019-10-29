a28c94d115f7d6330eda18d73c89a3a12b6320ac
marcelopilone <mdpilone@gmail.com>
2019-10-29 14:38:16 -0300

    hasta donde ahi

--- main.py
+++ main.py
@@ -1,14 +1,17 @@
 from kivy.app import App
 from kivymd.theming import ThemeManager
 from kivy.uix.label import Label
+from kivy.properties import ObjectProperty
+from kivy.uix.widget import Widget
+
 #from kivy.uix.behaviors.button import Button
 
 
 class MainApp(App):
     theme_cls = ThemeManager()
 
-    def screen1Local(self):
-    	print('asdas')
+    def screen1Local(self,*args):
+        print(self.email_uno.text)
 
 class screen1():
     def build(self):
